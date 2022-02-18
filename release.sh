1355bb293de251f9516696f73cb8ee71a9954c91
Mihai Doarna <mihaidoarna@improbable.io>
2022-02-18 14:31:42 +0200

    test

--- release.sh
+++ release.sh
@@ -2,7 +2,8 @@
 
 # prepare SSH key for accessing internal IO repos
 mkdir ~/.ssh
-echo "${INPUT_TFCD_SSH_ACCESS}" > ~/.ssh/id_rsa
+# echo "${INPUT_TFCD_SSH_ACCESS}" > ~/.ssh/id_rsa
+echo "${INPUT_GITHUB_TOKEN}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 
