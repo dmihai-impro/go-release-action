f58832769799c0d0485936912f1d7190dd2ad33e
Mihai Doarna <mihaidoarna@improbable.io>
2022-02-18 18:48:42 +0200

    remove ssh-add

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ mkdir ~/.ssh
 echo "${INPUT_TFCD_SSH_ACCESS}" > ~/.ssh/id_rsa
 echo "${INPUT_KNOWN_HOSTS}" > ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
+# ssh-add ~/.ssh/id_rsa
 
 git config --global url.org-32199913@github.com:.insteadOf "https://github.com/improbable"
 export GOPRIVATE="github.com/improbable/*"
